71a7d1151bfdfe57bcccec4d17dc258a437cd875
cgtx <carl@carlgeorge.us>
2014-03-03 16:07:33 -0600

    fetch all ips

--- listall.py
+++ listall.py
@@ -13,27 +13,62 @@ import pyrax
 import prettytable
 
 
+def get_ipv4(ips):
+    for ip in ips:
+        if ip.get('version') == 4:
+            return ip.get('addr')
+
+
+def server_data(server):
+    data = {}
+    addresses = server.addresses
+    private = get_ipv4(addresses.pop('private'))
+    public = get_ipv4(addresses.pop('public'))
+    if addresses:
+        x, others = addresses.popitem()
+        other = get_ipv4(others)
+    else:
+        other = None
+
+    data['uuid'] = server.id
+    data['name'] = server.name
+    data['access'] = server.accessIPv4
+    data['public'] = public
+    data['private'] = private
+    data['other'] = other
+    data['rc'] = server.metadata.get('rackconnect_automation_status')
+    return data
+
+
 def create_table(username, apikey):
     pyrax.set_credentials(username, apikey)
     serverlist = pyrax.cloudservers.list()
     output = prettytable.PrettyTable(['UUID',
-                                      'NAME',
-                                      'PUBLIC IP',
-                                      'PRIVATE IP',
-                                      'RACKCONNECT STATUS'])
+                                      'name',
+                                      'accessIPv4',
+                                      'public',
+                                      'servicenet',
+                                      'other',
+                                      'RackConnect status'])
     for server in serverlist:
-        output.add_row([server.id,
-                        server.name,
-                        server.accessIPv4,
-                        server.addresses.get('private')[0].get('addr'),
-                        server.metadata.get('rackconnect_automation_status')])
+        data = server_data(server)
+        output.add_row([data['uuid'],
+                        data['name'],
+                        data['access'],
+                        data['public'],
+                        data['private'],
+                        data['other'],
+                        data['rc']])
     output.align = 'l'
-    output.sortby = 'NAME'
+    output.sortby = 'name'
     return output
 
 
 def main():
     pyrax.set_setting('identity_type', 'rackspace')
+    # flavors = {
+    # }
+
     # keys are ddis
     credentials = {
         111111: {'username': 'account1',
